b65769f656053f76b177dc5e1f26608de02a7a9f
solarezz <122013283+solarezz@users.noreply.github.com>
2024-07-31 16:24:23 +0500

    create

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from pypresence import Presence
+import time
+
+client_id = "1268152698667008010"  # Replace this with your own client id
+
+r = requests.get('https://api.alt-mp.com/servers/dallas')
+count_players = r.json()['playersCount']
+
+rpc = Presence(client_id)
+rpc.connect()
+
+
+rpc.update(
+    state="Твой путь - твои правила",
+    details=f'Игроков на сервере: {count_players}',
+    large_image="majmax",
+    large_text="https://majestic-rp.ru",
+    start=time.time()
+)
+
+try:
+    while True:
+        time.sleep(15)
+except KeyboardInterrupt:
+    print("Отключение...")
+    rpc.close()
